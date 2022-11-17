8fb8b7b9ad1239e1110a36e947189341ab49003a
giovannidebug982 <114904759+giovannidebug982@users.noreply.github.com>
2022-11-17 13:46:03 +0100

    . funz

---
+++ app_News/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import
+app_name="app_News"
+
+urlpatterns=[
+    path('',,name=""),
+    
+
+
+]  
--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     "prima_app",
     "seconda_app",
+    "app_News",
 ]
 
 MIDDLEWARE = [
@@ -60,6 +61,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'prima_app/templates'),
                  os.path.join(BASE_DIR, 'seconda_app/templates'),
+                 os.path.join(BASE_DIR, 'app_News/templates'),
                  os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
